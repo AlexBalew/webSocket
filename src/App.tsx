9249d60cc7d087c33896b813cfa0481adf0c925b
AlexBalew <balew48@gmail.com>
2022-01-14 16:56:09 +0300

    chat window

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [messages, setMessages] = useState([
+        {message: 'hey hey Kate', id: 123948, user: {id: 'jnjkfvlf', name: 'Alex'}},
+        {message: 'hello Alex', id: 456, user: {id: '35hyhy', name: 'Kate'}}
+    ])
+
+
+    return (
+        <div className="App">
+            <div className='chatWindow'>
+                {messages.map(({message, id, user}) => {
+                    return <div key={id}>
+                        <b>{user.name}</b> : {message}
+                        <hr/>
+                    </div>
+                })}
+            </div>
+            <div className= 'messageBlock'>
+                <textarea >
+
+                </textarea>
+                <button>
+                    Send
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
